258ed5c20d0f15bab2eaa3f45e4ae512523e6130
Vojtech Novak <vonovak@gmail.com>
2024-05-29 21:55:33 +0200

    fix: build issue (#56)

--- src/spec/SignInButtonNativeComponent.ts
+++ src/spec/SignInButtonNativeComponent.ts
@@ -7,7 +7,7 @@ import type {
 } from 'react-native/Libraries/Types/CodegenTypes';
 
 interface EventParams {}
-interface NativeProps extends ViewProps {
+export interface NativeProps extends ViewProps {
   onPress?: BubblingEventHandler<EventParams>;
   disabled?: WithDefault<boolean, false>;
   color?: WithDefault<'dark' | 'light', 'light'>;
